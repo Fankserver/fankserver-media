ab6be35e1758e1513e5e7af955c58cb47095037f
Florian Kinder <Florian.Kinder@fankserver.com>
2015-09-14 18:57:12 +0200

    cleanup

--- index.js
+++
@@ -1,6 +0,0 @@
-var angular2_1 = require('angular2/angular2');
-var router_1 = require('angular2/router');
-var app_1 = require('./app/app');
-angular2_1.bootstrap(app_1.App, [
-    router_1.routerInjectables
-]);
--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import { bootstrap } from 'angular2/angular2';
-import { routerInjectables } from 'angular2/router';
+/// <reference path="typings/angular2/angular2.d.ts"/>
+/// <reference path="typings/angular2/router.d.ts"/>
+import { bootstrap } from "angular2/angular2";
+import { ROUTER_BINDINGS } from "angular2/router";
 
 import { App } from './app/app';
 
 bootstrap(
 	App,
 	[
-		routerInjectables
+		ROUTER_BINDINGS
 	]
 );
